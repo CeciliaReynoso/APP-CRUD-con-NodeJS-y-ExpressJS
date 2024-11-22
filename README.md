86b3245011586e49903ab1c21ee9d4a936f0518c
ceciliareynoso <cecilia.reynosom@outlook.com>
2024-11-21 21:29:35 -0500

    Modificación de Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # APP-CRUD-con-NodeJS-y-ExpressJS
+Aplicación tipo CRUD para mantener un repertorio de canciones de los estudiantes de una escuela de musica. 
+El sitio web del frontend puede realizar consultas a nuestra API REST local.  El backend proporciona las rutas para lo siguiente:
+● POST /canciones : Recibe los datos correspondientes a una canción y la agrega al repertorio.
+● GET /canciones : Devuelve un JSON con las canciones registradas en el repertorio
+● PUT /canciones/:id : Recibe los datos de una canción que se desea editar y la actualiza manipulando el JSON local.
+● DELETE /canciones/:id : Recibe por queryString el id de una canción y la elimina delrepertorio.
+
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const cors = require('cors');
+
+app.listen(5000, () => console.log("¡Servidor encendido!"));
+app.use(cors());
+app.use(express.json());
+
+// Devuelve un JSON con las canciones registradas en el repertorio
+app.get("/canciones", (req, res) => {
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf8"));
+    res.json(canciones);
+});
+
+// Recibe los datos correspondientes a una canción y la agrega al repertorio
+app.post("/canciones", (req, res) => {
+    const cancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf8"));
+    canciones.push(cancion);
+    fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
+    res.send("¡Canción agregada con éxito!");
+});
+
+// Recibe los datos de una canción que se desea editar y la actualiza manipulando el JSON local
+app.put("/canciones/:id", (req, res) => {
+    const { id } = req.params;
+    const cancion = req.body;
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf8"));
+    const index = canciones.findIndex(c => c.id == id);
+    canciones[index] = cancion;
+    fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
+    res.send("¡Canción modificada con éxito!");
+});
+
+// Recibe por queryString el id de una canción y la elimina del repertorio
+app.delete("/canciones/:id", (req, res) => {
+    const { id } = req.params;
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf8"));
+    const index = canciones.findIndex(c => c.id == id);
+    canciones.splice(index, 1);
+    fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
+    res.send("¡Canción eliminada con éxito!");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
